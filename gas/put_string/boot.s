ff07a33e1f7eddfe3b1ee2634c83aac15dacfc03
b1u3 <bp16071@shibaura-it.ac.jp>
2019-05-09 23:53:51 +0900

    print関数追加した

---
+++ gas/put_string/boot.s
@@ -0,0 +1,54 @@
+.code16
+.global init
+
+/* For FAT12 */
+init:
+jmp start	
+BS_jmpBoot2:    .byte	0x90
+BS_OEMName:	    .ascii  "MyOS    "
+BPB_BytsPerSec: .word	0x0200		
+BPB_SecPerClus:	.byte	0x01		
+BPB_RsvdSecCnt:	.word	0x0001		
+BPB_NumFATs:	.byte	0x02		
+BPB_RootEntCnt:	.word	0x00E0
+BPB_TotSec16:	.word	0x0B40
+BPB_Media:	    .byte	0xF0
+BPB_FATSz16:	.word	0x0009
+BPB_SecPerTrk:	.word	0x0012
+BPB_NumHeads:	.word	0x0002
+BPB_HiddSec:	.int	0x00000000
+BPB_TotSec32:	.int	0x00000000
+
+BS_DrvNum:	    .byte	0x00
+BS_Reserved1:	.byte	0x00
+BS_BootSig:	    .byte	0x29
+BS_VolID:	    .int	0x20190502
+BS_VolLab:	    .ascii	"MyOS       "
+BS_FilSysType:	.ascii	"FAT12   "
+
+start:
+  mov $msg, %si
+  hlt
+
+/* function print: print string, set ptr of stringz to %si */
+print:
+	push %ax
+	push %bx
+ 	mov $0x0e, %ah
+ 	mov $0x00, %bh
+ 	mov $0x0f, %bl
+print_char:
+ 	lodsb
+ 	cmp $0, %al
+ 	je done
+ 	int $0x10
+ 	jmp print_char
+done:
+	pop %bx
+	pop %ax
+	ret
+
+msg: .asciz "Hello world!"
+
+.fill 510-(.-init), 1, 0 # add zeroes to make it 510 bytes long
+.word 0xaa55 # magic bytes that tell BIOS that this is bootable
